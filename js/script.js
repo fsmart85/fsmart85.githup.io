bd37534c039e6aca00d62d935e103ff5a3538284
Farruh <40520271+fsmart85@users.noreply.github.com>
2018-11-29 00:06:41 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(function () {
 
     $(".nav_menu li a").click(function () {
         var elementClick = $(this).attr("href");
-        var destination = $(elementClick).offset().top;
+        var destination = $(elementClick).offset().top-50;
         jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
         return false;
     });
@@ -14,9 +14,35 @@ $(function () {
         $('body, html').animate({'scrollTop': 0}, 800)
     });
 
+    $('.img_list li a').on('click', function (e) {
+        e.preventDefault();
+        $('.imgBox img').attr("src", $(this).attr("href"));
+        var ptext = $(this).siblings('.img_link').find('p').text();
+        $('.imgBox .imgBox_title p').text(ptext);
+        var h4text = $(this).siblings('.img_link').find('h4').text();
+        $('.imgBox .imgBox_title h4').text(h4text);
+        var ahref = $(this).siblings('.img_link').find('a').attr("href");
+        $('.imgBox .imgBox_title .btn_go').attr("href", ahref);
+    });
+
+    $('.toggle-icon').on('click', function () {
+        $('.nav_menu').slideToggle();
+        $('.toggle-icon .fas').toggleClass('fa-times');
+    });
+
+    $('.btn_submit').on('click', function () {
+        $('input:not(input.btn_submit), textarea').val("");
+        alert("Ваше сообщение не отправлено!" +
+            " Отправте по email или звоните.");
+    });
+
+    $('input:not(input.btn_submit), textarea').on('focus', function () {
+        $(this).attr("placeholder", "");
+    });
+
     $(window).scroll(function () {
         if($(window).scrollTop() > 100){
-            $('.btn_Up').addClass('active');
+            $('.btn_Up').addClass('active')
             $('.nav_top').addClass('fixed')
         }else {
             $('.btn_Up').removeClass('active');
@@ -24,11 +50,11 @@ $(function () {
         }
 
         var st = $(window).scrollTop();
-            $('.banner_img').css('transform', 'translate(0%, '+ st * 0.60 + 'px');
+        $('.banner_img').css('transform', 'translate(0%, '+ st * 0.60 + 'px');
 
         $('header[id], .sec_content[id]').each(function () {
             var id = $(this).attr("id");
-            if($(this).offset().top-100 < $(window).scrollTop()){
+            if($(this).offset().top-50 < $(window).scrollTop()){
                 $('.nav_menu li a[href="#'+id+'"]').parent().addClass('active').siblings().removeClass('active');
 
             }
