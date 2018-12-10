79fe282d41cf41ef13c402685e9f28e8c5c0928f
Farruh <40520271+fsmart85@users.noreply.github.com>
2018-12-10 18:59:12 +0500

    Add files via upload

---
+++ talent/js/1.js
@@ -0,0 +1,21 @@
+$(function(){
+	var menu = $('.header_menu ul li');
+	menu.on('click', function(e){
+		e.preventDefault();
+		menu.removeClass('active').filter(this).addClass('active');
+
+	})
+	// var img = $('.slider');
+	// 	for(img of imgs){
+	// 		img.removeClass('active').filter(this).addClass('active');
+
+	// 	}
+
+	$('.fa').on('click', function () {
+		$('ul').slideToggle(300, function () {
+			if($(this).css('display') === 'none'){
+                $(this).removeAttr('style');
+			}
+        })
+    })
+});
